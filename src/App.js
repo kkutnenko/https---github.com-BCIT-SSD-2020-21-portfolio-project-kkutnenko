bda117f249401785d3c027fdab6b825b121d489a
Kirill Kutnenko <kkutnenko@my.bcit.ca>
2020-11-26 12:12:37 -0800

    some changes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          ddedd
         >
           Learn React
         </a>
