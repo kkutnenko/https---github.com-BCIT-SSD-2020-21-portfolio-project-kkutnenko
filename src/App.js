4cf27350277e40bddc79245fccd49e86ce5a024a
Kirill Kutnenko <kkutnenko@my.bcit.ca>
2020-11-26 14:14:13 -0800

    Skeleton created closes #3

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          ddedd
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className = "row">
+          <div className="col-3 app_sidebar">
+              side
+          </div>
+          
+
+          <div className = "col-9 app_main">
+              main
+          </div>
+        </div>  
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery';
+import 'popper.js';
+import 'bootstrap/dist/js/bootstrap';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
